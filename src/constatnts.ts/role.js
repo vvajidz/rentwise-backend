283b1e9d049fe8c38d1d9691649077236e144a4f
Abdul Vajid <vvajidz56@gmail.com>
2025-10-28 14:52:58 +0530

    rentwise backend pushing

--- src/constatnts.ts/role.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ROLES = void 0;
-exports.ROLES = {
-    TENANT: "tenant",
-    OWNER: "owner",
-    ADMIN: "admin"
-};
--- src/routes/bookingRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const booking_1 = require("../controllers/booking");
-const getme_1 = require("../middleware/getme");
-const router = (0, express_1.Router)();
-router.post('/request', getme_1.verifyToken, booking_1.createBookingRequest);
-exports.default = router;
